4389ce82da4e5b995ae199ba2d3026ff7277dfe5
Minh Cong Ho <ho.cong.minh1996@gmail.com>
2017-12-03 20:48:29 -0500

    Update Player Object
    Exploding effect when hit and randomize the key and shape and color (need to change to player decision later)

--- Musical-Keys/Assets/Scripts/ObjectSizeChange.cs
+++ Musical-Keys/Assets/Scripts/ObjectSizeChange.cs
@@ -7,15 +7,27 @@ public class ObjectSizeChange : MonoBehaviour {
     private Color color;
     private SpriteRenderer sr;
     public TextMesh tm;
+    public ParticleSystem ps;
+    public Sprite[] spriteList;
+    private int spriteIndex;
     private bool destroy;
     private bool startPressing;
+    private int keyIndex;
+    private char[] possibleKeys = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
 	// Use this for initialization
 	void Start () {
         sr = GetComponent<SpriteRenderer>();
-        color = sr.color;
+        color = new Color(Random.Range(0, 255)/255.0f, Random.Range(0, 255) / 255.0f, Random.Range(0, 255) / 255.0f, 150.0f/255.0f);
+        sr.color = color;
         destroy = true;
         startPressing = false;
+        spriteIndex = Random.Range(0, spriteList.Length-1);
+        sr.sprite = spriteList[spriteIndex];
+        keyIndex = Random.Range(0, possibleKeys.Length-1);
+        tm.text = possibleKeys[keyIndex].ToString();
     }
 	
 	// Update is called once per frame
@@ -23,9 +35,10 @@ public class ObjectSizeChange : MonoBehaviour {
         StartCoroutine(ChangeSizeAndBrightness());
         if (startPressing)
         {
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.inputString == possibleKeys[keyIndex].ToString().ToLower())
             {
-                destroy = false;
+                ps.Play();
+                gameObject.SetActive(false);
             }
         }
 	}
@@ -45,7 +58,7 @@ public class ObjectSizeChange : MonoBehaviour {
         yield return new WaitForSeconds(1);
         if (destroy)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
